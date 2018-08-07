7ecbc07c89105006c32c9633a6f11f6a9f797fdd
Alwyn Kik <alwyn@firelay.com>
2018-08-07 15:45:12 +0200

    Load properties from file given as first program parameter and convert to json string

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,60 @@
-import pl.jalokim.propertiestojson.util.PropertiesToJsonConverter;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
-        Properties properties = new Properties();
-        InputStream input = null;
-
-        try {
-
-            input = new FileInputStream("/tmp/my.properties");
-
-            // load a properties file
-            properties.load(input);
-
-            // get the property value and print it out
-            //System.out.println(properties.getProperty("ldap.user.mappings.0"));
-
-            // convert to json
-            String json = new PropertiesToJsonConverter().parseToJson(input);
-            //System.out.println(json.length());
-
-            // manual
-            Set<String> keys = properties.stringPropertyNames();
-            for (String key : keys) {
-                System.out.println(key + " : " + properties.getProperty(key));
-            }
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        String inputFile = "/dev/null";
+        if (args.length > 0) {
+            inputFile = args[0];
+            System.out.println(inputFile);
         }
 
+        Properties properties = readProperties(inputFile);
+        JSONObject jsonProperties = convertPropertiesToJson(properties);
+        returnJsonString(jsonProperties);
+    }
+
+    private static Properties readProperties(String inputFile) {
+        Properties properties = new Properties();
+        InputStream input;
+
+        try {
+            input = new FileInputStream("/tmp/my.properties");
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(2);
+        }
+
+        return properties;
+    }
+
+    private static JSONObject convertPropertiesToJson(Properties properties) {
+        JSONObject jsonObject = null;
+
+        try {
+            jsonObject = new JSONObject(properties);
+        } catch (JSONException jex) {
+            jex.printStackTrace();
+            System.exit(3);
+        }
+
+        return jsonObject;
+    }
+
+    private static void returnJsonString(JSONObject json) {
+        String jsonString = json.toString(4);
+
+        /*try (FileWriter file = new FileWriter("/tmp/my.json")) {
+            file.write(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        System.out.println(jsonString);
     }
 }
